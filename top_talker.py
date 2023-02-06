da6b0c4131004820657e4441111d4648d1618053
spardkar <satish_paradkar@yahoo.com>
2023-02-06 11:15:27 -0500

    Add files via upload

---
+++ top_talker.py
@@ -0,0 +1,40 @@
+'''
+Reading CSV and cleaning the value
+'''
+import csv
+from collections import defaultdict
+from pprint import pprint
+
+total_input = 0
+total_output = 0
+top_talkers = []
+
+devices = defaultdict(list)
+file_path = "/Users/satishparadkar/Documents/Ansible-REPO/PycharmProjects/pythonProject1/stats.csv"
+with open(file_path, 'r') as fh:
+    csv_reader = csv.DictReader(fh)
+
+    for sw in csv_reader:
+
+        if not sw['output'].isnumeric():
+            sw['output'] = 0
+
+        if not sw['input'].isnumeric():
+            sw['input'] = 0
+
+        if sw['device'] not in devices:
+            devices[sw['device']] = [0, 0]
+
+        if sw['device'] in devices:
+            devices[sw['device']][0] = devices[sw['device']][0] + int(sw['input'])
+            devices[sw['device']][1] = devices[sw['device']][1] + int(sw['output'])
+
+        top_talkers.append(sw)
+
+print('Top talkers devices based on input traffic:')
+pprint(sorted(top_talkers, key=lambda x: int(x['input']), reverse=False))
+
+print('\n', 'Total traffic per device:')
+for device, traffic in devices.items():
+    print(device, traffic)
+    print(f'Total traffic on {device} is {traffic[0] + traffic[1]}')
